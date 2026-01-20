e12537178db76213f9a4567bd3ea491e2021f9ee
Wilder Caicedo <ghocho88@gmail.com>
2026-01-20 17:57:28 -0500

    chore: configure image domains for production

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // Google Auth Avatars
+      },
+      {
+        protocol: 'https',
+        hostname: 'mnkweisggxelscoqvwnd.supabase.co', // Supabase Storage
+      },
+    ],
+  },
 };
 
 export default nextConfig;
